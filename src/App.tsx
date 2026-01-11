a9012da089c7f12a6b039249ca49d586854b2709
Muniru Adam <madam31@academic.rrc.ca>
2026-01-11 17:06:31 -0600

    Updated heading for live site

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>My First React App! 🚀</h1>
+      <h1>My Live React App! 🌍 Now on the Internet!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
